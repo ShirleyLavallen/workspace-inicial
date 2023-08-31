afd9a855fd56dcd745c2e8e3a150415f197a9c30
alexist <nahutxd@gmail.com>
2023-08-31 12:25:07 -0300

    Búsqueda de lista de productos

--- docs/js/products.js
+++ docs/js/products.js
@@ -2,12 +2,14 @@ const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.jso
 
 const container = document.getElementById("products-list");
 const categoryTitle = document.getElementById('category-title')
+const searchInput = document.getElementById("searchV");
 
 function showCategoryName(name){
     categoryTitle.innerText = name;
 }
 
 function showProducts(products) {
+    container.innerHTML = "";
     for (const product of products) {
         container.innerHTML += `
             <div class="col-4">
@@ -28,8 +30,32 @@ fetch(DATA_URL)
   .then(response => response.json())
   .then(data => {
     showCategoryName(data.catName); 
-    showProducts(data.products);
-  })
+    const originalProducts = data.products; // lista de los productos
+    showProducts(originalProducts); 
+
+    // busqueda de productos
+    searchInput.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+          const searchTerm = searchInput.value.trim().toLowerCase();
+  
+          if (!searchTerm) {
+            return showProducts(originalProducts);
+          }
+
+          const filteredProducts = originalProducts.filter(product =>
+              product.name.toLowerCase().split(" ").includes(searchTerm) ||
+              product.description.toLowerCase().split(" ").includes(searchTerm)
+          );
+  
+          if (filteredProducts.length) {
+            showProducts(filteredProducts);
+          } else {
+            container.innerHTML = 
+             `<div class= notFound> <p>No se encontraron resultados</p> </div>`;
+          }
+      }
+  });
+})
   .catch(error => {
     console.error("Error:", error);
   });
