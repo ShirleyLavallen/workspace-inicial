587b197d8d654eeffb111d87fd270da3e4e75925
Joaquin Fuentes <joacofd2002.jf@gmail.com>
2023-09-12 17:54:48 -0300

    Mostrar comentario

--- docs/js/product-info.js
+++ docs/js/product-info.js
@@ -1,3 +1,5 @@
+const commentsContainer = document.getElementById("comment")
+
 function getProductId(){
     return localStorage.getItem('selectedProduct');
   }
@@ -6,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const productId = getProductId();
 
 
-    alert(productId);
 
 
     fetch(PRODUCT_INFO_URL + productId + ".json")
@@ -82,29 +83,66 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error("Error:", error);
     });
 
-
-
-
     // fetch a json de los comentarios de los productos
 
-    
     fetch(URL_COMENTARIOS + productId + ".json")
     .then(response => response.json())
-    .then(comment => { 
-      
+    .then(comments => {
 
-      // recorrer cada cometario de la lista 
-
-      for (const i of comment) {
-      
-      let producto = i.product;
-      let description = i.description;
-      let dateTime = i.dateTime;
-      let username = i.user;
-      let score = i.score;
-      
-      // console.log (producto,description,dateTime,username,score);
-      }
+      showComments(comments);
     })
+      
+ })
 
-})
+ const currentCommentsArray = [];
+
+ function showComments(comments) {
+  let htmlCommentsToAppend = "";
+  for (let i = 0; i < comments.length; i++) {
+    let comment = comments[i];
+      
+      let producto = comment.product;
+      let description = comment.description;
+      let dateTime = comment.dateTime;
+      let username = comment.user;
+      let score = comment.score;
+    
+        
+    htmlCommentsToAppend += `
+    <section style="background-color: #f7f6f6;">
+      <div class="container my-5 py-5 text-dark">
+       <div class="row d-flex justify-content-center">
+        <div class="col-md-12 col-lg-10 col-xl-8"
+
+         <div class="card mb-3">
+           <div class="card-body">
+             <div class="d-flex flex-start">
+               <div class="w-100">
+                 <div class="d-flex justify-content-between align-items-center mb-3">
+                   <h6 class="text-primary fw-bold mb-0">
+                     ${username}
+                     <span class="text-dark ms-2">${description}
+                   </h6>
+                   <p class="mb-0">${dateTime}</p>
+                 </div>
+                 <div class="d-flex justify-content-between align-items-center">
+                   <p class="small mb-0" style="color: #aaa;">
+                     <a href="#!" class="link-grey">Remover</a> •
+                     <a href="#!" class="link-grey">Editar</a> •
+                     <a href="#!" class="link-grey">Traducir</a>
+                   </p>
+                   <div class="d-flex flex-row">
+                     ${score}<i class="fas fa-star text-warning me-2"></i>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+        </div>
+      </div>
+     </div>
+    </section>`; 
+  } 
+  commentsContainer.innerHTML = htmlCommentsToAppend;
+}
