3e3232dc33942650f492d2c682ce31c92b27f1cf
j-martinez-s <jimemartinez0506@gmail.com>
2023-09-26 03:10:46 -0300

    iconos menú desplegable v2

---
+++ docs/js/my-profile.js
@@ -0,0 +1,30 @@
+//verificación del login
+
+document.addEventListener("DOMContentLoaded", function () {
+    const userSes = getSessionData("username");
+  
+    if (!userSes) {
+      alert("Por favor, registrate");
+      window.location.href = "login.html";
+    }
+  });
+  
+  function getSessionData(usrname) {
+    return localStorage.getItem(usrname);
+  } 
+  
+  //Usuario en la esquina superior derecha
+  
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    
+    const container = document.getElementById("dataUsuario");
+    const usuario = localStorage.getItem("username");
+    
+      container.textContent = usuario;
+  })
+  
+  let cerrar = document.getElementById("cerrarsesion"); //borrar usuario al cerrar sesion
+  cerrar.addEventListener("click", function () {
+    localStorage.removeItem("username"); 
+  });
